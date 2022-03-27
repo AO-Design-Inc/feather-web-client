c29bc4c314df69a2f4ce21776d282e024bbe4342
Yang Xi Gao <36776982+joGao01@users.noreply.github.com>
2022-03-27 15:15:02 -0400

    this needs to be hosted somewhere - has no user actions yet

--- script.js
+++ script.js
@@ -1,43 +1,123 @@
-let paramsString =
-  "server=bestie&offer=%257B%2522type%2522%253A%2522offer%2522%252C%2522sdp%2522%253A%2522v%253D0%255Cr%255Cno%253D-%25201227339979152991805%25202%2520IN%2520IP4%2520127.0.0.1%255Cr%255Cns%253D-%255Cr%255Cnt%253D0%25200%255Cr%255Cna%253Dgroup%253ABUNDLE%25200%25201%255Cr%255Cna%253Dextmap-allow-mixed%255Cr%255Cna%253Dmsid-semantic%253A%2520WMS%25209CrbMDagaHbJCSlUUX0BVbE4u278hJdIFEWH%255Cr%255Cnm%253Dvideo%25209%2520UDP%252FTLS%252FRTP%252FSAVPF%252096%252097%252098%252099%2520100%2520101%2520102%2520121%2520127%2520120%2520125%2520107%2520108%2520109%2520124%2520119%2520123%2520117%252035%252036%2520114%2520115%2520116%252062%2520118%255Cr%255Cnc%253DIN%2520IP4%25200.0.0.0%255Cr%255Cna%253Drtcp%253A9%2520IN%2520IP4%25200.0.0.0%255Cr%255Cna%253Dice-ufrag%253AVgpQ%255Cr%255Cna%253Dice-pwd%253AVpefn1aXl5%252FTDYqTeyKre58X%255Cr%255Cna%253Dice-options%253Atrickle%255Cr%255Cna%253Dfingerprint%253Asha-256%2520C0%253A61%253A5B%253ABC%253A73%253A87%253A54%253A87%253AEE%253A71%253A01%253A33%253AD0%253A1F%253ADF%253A8C%253A67%253A1F%253AAF%253A21%253A24%253A4D%253A17%253AB5%253A97%253AD3%253A80%253A49%253ACB%253A91%253AB8%253AF5%255Cr%255Cna%253Dsetup%253Aactpass%255Cr%255Cna%253Dmid%253A0%255Cr%255Cna%253Dextmap%253A1%2520urn%253Aietf%253Aparams%253Artp-hdrext%253Atoffset%255Cr%255Cna%253Dextmap%253A2%2520http%253A%252F%252Fwww.webrtc.org%252Fexperiments%252Frtp-hdrext%252Fabs-send-time%255Cr%255Cna%253Dextmap%253A3%2520urn%253A3gpp%253Avideo-orientation%255Cr%255Cna%253Dextmap%253A4%2520http%253A%252F%252Fwww.ietf.org%252Fid%252Fdraft-holmer-rmcat-transport-wide-cc-extensions-01%255Cr%255Cna%253Dextmap%253A5%2520http%253A%252F%252Fwww.webrtc.org%252Fexperiments%252Frtp-hdrext%252Fplayout-delay%255Cr%255Cna%253Dextmap%253A6%2520http%253A%252F%252Fwww.webrtc.org%252Fexperiments%252Frtp-hdrext%252Fvideo-content-type%255Cr%255Cna%253Dextmap%253A7%2520http%253A%252F%252Fwww.webrtc.org%252Fexperiments%252Frtp-hdrext%252Fvideo-timing%255Cr%255Cna%253Dextmap%253A8%2520http%253A%252F%252Fwww.webrtc.org%252Fexperiments%252Frtp-hdrext%252Fcolor-space%255Cr%255Cna%253Dextmap%253A9%2520urn%253Aietf%253Aparams%253Artp-hdrext%253Asdes%253Amid%255Cr%255Cna%253Dextmap%253A10%2520urn%253Aietf%253Aparams%253Artp-hdrext%253Asdes%253Artp-stream-id%255Cr%255Cna%253Dextmap%253A11%2520urn%253Aietf%253Aparams%253Artp-hdrext%253Asdes%253Arepaired-rtp-stream-id%255Cr%255Cna%253Dsendrecv%255Cr%255Cna%253Dmsid%253A9CrbMDagaHbJCSlUUX0BVbE4u278hJdIFEWH%252055100f43-14e8-4fcd-842e-d3080c0fbb01%255Cr%255Cna%253Drtcp-mux%255Cr%255Cna%253Drtcp-rsize%255Cr%255Cna%253Drtpmap%253A96%2520VP8%252F90000%255Cr%255Cna%253Drtcp-fb%253A96%2520goog-remb%255Cr%255Cna%253Drtcp-fb%253A96%2520transport-cc%255Cr%255Cna%253Drtcp-fb%253A96%2520ccm%2520fir%255Cr%255Cna%253Drtcp-fb%253A96%2520nack%255Cr%255Cna%253Drtcp-fb%253A96%2520nack%2520pli%255Cr%255Cna%253Drtpmap%253A97%2520rtx%252F90000%255Cr%255Cna%253Dfmtp%253A97%2520apt%253D96%255Cr%255Cna%253Drtpmap%253A98%2520VP9%252F90000%255Cr%255Cna%253Drtcp-fb%253A98%2520goog-remb%255Cr%255Cna%253Drtcp-fb%253A98%2520transport-cc%255Cr%255Cna%253Drtcp-fb%253A98%2520ccm%2520fir%255Cr%255Cna%253Drtcp-fb%253A98%2520nack%255Cr%255Cna%253Drtcp-fb%253A98%2520nack%2520pli%255Cr%255Cna%253Dfmtp%253A98%2520profile-id%253D0%255Cr%255Cna%253Drtpmap%253A99%2520rtx%252F90000%255Cr%255Cna%253Dfmtp%253A99%2520apt%253D98%255Cr%255Cna%253Drtpmap%253A100%2520VP9%252F90000%255Cr%255Cna%253Drtcp-fb%253A100%2520goog-remb%255Cr%255Cna%253Drtcp-fb%253A100%2520transport-cc%255Cr%255Cna%253Drtcp-fb%253A100%2520ccm%2520fir%255Cr%255Cna%253Drtcp-fb%253A100%2520nack%255Cr%255Cna%253Drtcp-fb%253A100%2520nack%2520pli%255Cr%255Cna%253Dfmtp%253A100%2520profile-id%253D2%255Cr%255Cna%253Drtpmap%253A101%2520rtx%252F90000%255Cr%255Cna%253Dfmtp%253A101%2520apt%253D100%255Cr%255Cna%253Drtpmap%253A102%2520H264%252F90000%255Cr%255Cna%253Drtcp-fb%253A102%2520goog-remb%255Cr%255Cna%253Drtcp-fb%253A102%2520transport-cc%255Cr%255Cna%253Drtcp-fb%253A102%2520ccm%2520fir%255Cr%255Cna%253Drtcp-fb%253A102%2520nack%255Cr%255Cna%253Drtcp-fb%253A102%2520nack%2520pli%255Cr%255Cna%253Dfmtp%253A102%2520level-asymmetry-allowed%253D1%253Bpacketization-mode%253D1%253Bprofile-level-id%253D42001f%255Cr%255Cna%253Drtpmap%253A121%2520rtx%252F90000%255Cr%255Cna%253Dfmtp%253A121%2520apt%253D102%255Cr%255Cna%253Drtpmap%253A127%2520H264%252F90000%255Cr%255Cna%253Drtcp-fb%253A127%2520goog-remb%255Cr%255Cna%253Drtcp-fb%253A127%2520transport-cc%255Cr%255Cna%253Drtcp-fb%253A127%2520ccm%2520fir%255Cr%255Cna%253Drtcp-fb%253A127%2520nack%255Cr%255Cna%253Drtcp-fb%253A127%2520nack%2520pli%255Cr%255Cna%253Dfmtp%253A127%2520level-asymmetry-allowed%253D1%253Bpacketization-mode%253D0%253Bprofile-level-id%253D42001f%255Cr%255Cna%253Drtpmap%253A120%2520rtx%252F90000%255Cr%255Cna%253Dfmtp%253A120%2520apt%253D127%255Cr%255Cna%253Drtpmap%253A125%2520H264%252F90000%255Cr%255Cna%253Drtcp-fb%253A125%2520goog-remb%255Cr%255Cna%253Drtcp-fb%253A125%2520transport-cc%255Cr%255Cna%253Drtcp-fb%253A125%2520ccm%2520fir%255Cr%255Cna%253Drtcp-fb%253A125%2520nack%255Cr%255Cna%253Drtcp-fb%253A125%2520nack%2520pli%255Cr%255Cna%253Dfmtp%253A125%2520level-asymmetry-allowed%253D1%253Bpacketization-mode%253D1%253Bprofile-level-id%253D42e01f%255Cr%255Cna%253Drtpmap%253A107%2520rtx%252F90000%255Cr%255Cna%253Dfmtp%253A107%2520apt%253D125%255Cr%255Cna%253Drtpmap%253A108%2520H264%252F90000%255Cr%255Cna%253Drtcp-fb%253A108%2520goog-remb%255Cr%255Cna%253Drtcp-fb%253A108%2520transport-cc%255Cr%255Cna%253Drtcp-fb%253A108%2520ccm%2520fir%255Cr%255Cna%253Drtcp-fb%253A108%2520nack%255Cr%255Cna%253Drtcp-fb%253A108%2520nack%2520pli%255Cr%255Cna%253Dfmtp%253A108%2520level-asymmetry-allowed%253D1%253Bpacketization-mode%253D0%253Bprofile-level-id%253D42e01f%255Cr%255Cna%253Drtpmap%253A109%2520rtx%252F90000%255Cr%255Cna%253Dfmtp%253A109%2520apt%253D108%255Cr%255Cna%253Drtpmap%253A124%2520H264%252F90000%255Cr%255Cna%253Drtcp-fb%253A124%2520goog-remb%255Cr%255Cna%253Drtcp-fb%253A124%2520transport-cc%255Cr%255Cna%253Drtcp-fb%253A124%2520ccm%2520fir%255Cr%255Cna%253Drtcp-fb%253A124%2520nack%255Cr%255Cna%253Drtcp-fb%253A124%2520nack%2520pli%255Cr%255Cna%253Dfmtp%253A124%2520level-asymmetry-allowed%253D1%253Bpacketization-mode%253D1%253Bprofile-level-id%253D4d001f%255Cr%255Cna%253Drtpmap%253A119%2520rtx%252F90000%255Cr%255Cna%253Dfmtp%253A119%2520apt%253D124%255Cr%255Cna%253Drtpmap%253A123%2520H264%252F90000%255Cr%255Cna%253Drtcp-fb%253A123%2520goog-remb%255Cr%255Cna%253Drtcp-fb%253A123%2520transport-cc%255Cr%255Cna%253Drtcp-fb%253A123%2520ccm%2520fir%255Cr%255Cna%253Drtcp-fb%253A123%2520nack%255Cr%255Cna%253Drtcp-fb%253A123%2520nack%2520pli%255Cr%255Cna%253Dfmtp%253A123%2520level-asymmetry-allowed%253D1%253Bpacketization-mode%253D0%253Bprofile-level-id%253D4d001f%255Cr%255Cna%253Drtpmap%253A117%2520rtx%252F90000%255Cr%255Cna%253Dfmtp%253A117%2520apt%253D123%255Cr%255Cna%253Drtpmap%253A35%2520AV1%252F90000%255Cr%255Cna%253Drtcp-fb%253A35%2520goog-remb%255Cr%255Cna%253Drtcp-fb%253A35%2520transport-cc%255Cr%255Cna%253Drtcp-fb%253A35%2520ccm%2520fir%255Cr%255Cna%253Drtcp-fb%253A35%2520nack%255Cr%255Cna%253Drtcp-fb%253A35%2520nack%2520pli%255Cr%255Cna%253Drtpmap%253A36%2520rtx%252F90000%255Cr%255Cna%253Dfmtp%253A36%2520apt%253D35%255Cr%255Cna%253Drtpmap%253A114%2520H264%252F90000%255Cr%255Cna%253Drtcp-fb%253A114%2520goog-remb%255Cr%255Cna%253Drtcp-fb%253A114%2520transport-cc%255Cr%255Cna%253Drtcp-fb%253A114%2520ccm%2520fir%255Cr%255Cna%253Drtcp-fb%253A114%2520nack%255Cr%255Cna%253Drtcp-fb%253A114%2520nack%2520pli%255Cr%255Cna%253Dfmtp%253A114%2520level-asymmetry-allowed%253D1%253Bpacketization-mode%253D1%253Bprofile-level-id%253D64001f%255Cr%255Cna%253Drtpmap%253A115%2520rtx%252F90000%255Cr%255Cna%253Dfmtp%253A115%2520apt%253D114%255Cr%255Cna%253Drtpmap%253A116%2520red%252F90000%255Cr%255Cna%253Drtpmap%253A62%2520rtx%252F90000%255Cr%255Cna%253Dfmtp%253A62%2520apt%253D116%255Cr%255Cna%253Drtpmap%253A118%2520ulpfec%252F90000%255Cr%255Cna%253Dssrc-group%253AFID%25203676754729%25202229054531%255Cr%255Cna%253Dssrc%253A3676754729%2520cname%253AlG885AcOiXz2Z9Kv%255Cr%255Cna%253Dssrc%253A3676754729%2520msid%253A9CrbMDagaHbJCSlUUX0BVbE4u278hJdIFEWH%252055100f43-14e8-4fcd-842e-d3080c0fbb01%255Cr%255Cna%253Dssrc%253A3676754729%2520mslabel%253A9CrbMDagaHbJCSlUUX0BVbE4u278hJdIFEWH%255Cr%255Cna%253Dssrc%253A3676754729%2520label%253A55100f43-14e8-4fcd-842e-d3080c0fbb01%255Cr%255Cna%253Dssrc%253A2229054531%2520cname%253AlG885AcOiXz2Z9Kv%255Cr%255Cna%253Dssrc%253A2229054531%2520msid%253A9CrbMDagaHbJCSlUUX0BVbE4u278hJdIFEWH%252055100f43-14e8-4fcd-842e-d3080c0fbb01%255Cr%255Cna%253Dssrc%253A2229054531%2520mslabel%253A9CrbMDagaHbJCSlUUX0BVbE4u278hJdIFEWH%255Cr%255Cna%253Dssrc%253A2229054531%2520label%253A55100f43-14e8-4fcd-842e-d3080c0fbb01%255Cr%255Cnm%253Dapplication%25209%2520UDP%252FDTLS%252FSCTP%2520webrtc-datachannel%255Cr%255Cnc%253DIN%2520IP4%25200.0.0.0%255Cr%255Cna%253Dice-ufrag%253AVgpQ%255Cr%255Cna%253Dice-pwd%253AVpefn1aXl5%252FTDYqTeyKre58X%255Cr%255Cna%253Dice-options%253Atrickle%255Cr%255Cna%253Dfingerprint%253Asha-256%2520C0%253A61%253A5B%253ABC%253A73%253A87%253A54%253A87%253AEE%253A71%253A01%253A33%253AD0%253A1F%253ADF%253A8C%253A67%253A1F%253AAF%253A21%253A24%253A4D%253A17%253AB5%253A97%253AD3%253A80%253A49%253ACB%253A91%253AB8%253AF5%255Cr%255Cna%253Dsetup%253Aactpass%255Cr%255Cna%253Dmid%253A1%255Cr%255Cna%253Dsctp-port%253A5000%255Cr%255Cna%253Dmax-message-size%253A262144%255Cr%255Cn%2522%257D";
+// server endpoints
+const signalingServerUrl =
+	"https://experimental.simplesignal.api.feather.systems";
+const send = signalingServerUrl + "/sendToPeerFromAnonymous";
 
-const signalingServerUrl = "https://simplesignal.api.feather.systems";
-const send = signalingServerUrl + "/sendToPeer";
+const self = "TEMP";
 
+// param values
+let senderid;
+let server;
+let offer;
+let iceServer;
+let max_X;
+let max_Y;
+
+// wrapper function for sending messages to server
 const sendMessage = async (peerId, jsonMessage) => {
-  fetch(`${send}?peer=${peerId}`, {
-    method: "POST",
-    body: JSON.stringify(jsonMessage),
-  });
+	fetch(`${send}?peer=${peerId}&senderid=${senderid}`, {
+		method: "POST",
+		body: JSON.stringify(jsonMessage),
+	});
 };
 
-const params = new Proxy(new URLSearchParams(paramsString), {
-  get: (searchParams, prop) => searchParams.get(prop),
+/***************** HANDLE SEARCH PARAMS *********************/
+
+const params = new Proxy(new URLSearchParams(window.location.search), {
+	get: (searchParams, prop) => searchParams.get(prop),
 });
 
-const server = params.server;
-const offer = JSON.parse(decodeURIComponent(params.offer));
-const connection = new RTCPeerConnection();
+if (params.server) {
+	server = params.server;
+	offer = JSON.parse(decodeURIComponent(params.offer));
+	iceServer = JSON.parse(decodeURIComponent(params.ice));
+	senderid = decodeURIComponent(params.serverid);
+	max_X = params.x;
+	max_Y = params.y;
 
-const sendAnswer = (program, offer, server) => {
-  connection
-    .setRemoteDescription(offer)
-    .then(() => connection.createAnswer())
-    .then((answer) => {
-      connection.setLocalDescription(answer);
-      console.log(answer);
-      sendMessage(server, {
-        sender: "TEMP",
-        type: "answer",
-        program: "",
-        body: answer,
-      });
-      /*window.opener.postMessage(
-        JSON.stringify({ sender: self, type: 'answer', program, body: answer, server })
-      );*/
-    });
-};
+	const video = document.getElementById("video");
+	const connection = new RTCPeerConnection({ iceServers: iceServer });
 
-sendAnswer("", offer, server);
+	// handle ice candidates
+	connection.onicecandidate = (e) => {
+		!e.candidate ||
+			sendMessage(server, {
+				sender: self,
+				type: "ice",
+				body: e.candidate,
+				server,
+			});
+	};
 
-sendMessage(server, {});
+	connection.oniceconnectionstatechange = (e) => {
+		console.log(connection.iceConnectionState);
+	};
+
+	const sendAnswer = (offer, server) => {
+		connection
+			.setRemoteDescription(offer)
+			.then(() => connection.createAnswer())
+			.then((answer) => {
+				connection.setLocalDescription(answer);
+				console.log(connection);
+				sendMessage(server, {
+					sender: self,
+					type: "answer",
+					body: answer,
+				});
+			});
+	};
+
+	sendAnswer(offer, server);
+
+	connection.ontrack = (e) => {
+		console.log("track detected");
+		console.log(e);
+		video.srcObject = e.streams[0];
+		const getVideoSize = () => {
+			const server_aspect_ratio = max_X / max_Y;
+			const clientWindowWidth = window.innerWidth;
+			const clientWindowHeight = window.innerHeight;
+
+			const client_aspect_ratio = clientWindowWidth / clientWindowHeight;
+
+			return client_aspect_ratio > server_aspect_ratio
+				? {
+						width: clientWindowHeight * server_aspect_ratio,
+						height: clientWindowHeight,
+				  }
+				: {
+						width: clientWindowWidth,
+						height: clientWindowWidth / server_aspect_ratio,
+				  };
+		};
+		const setVideoSize = () => {
+			const videoSize = getVideoSize();
+			video.style.width = String(videoSize.width) + "px";
+			video.style.height = String(videoSize.height) + "px";
+		};
+
+		//set video size initially
+		setVideoSize();
+
+		//video.srcObject = e.streams[0];
+
+		video.addEventListener(
+			"loadedmetadata",
+			() => {
+				setVideoSize();
+				// set window size to initial video size
+				const videoSize = getVideoSize();
+				window.resizeTo(videoSize.width, videoSize.height);
+
+				window.onresize = () => {
+					setVideoSize();
+				};
+
+				// benchmarking code
+				//window.opener.postMessage(JSON.stringify({ sender: self, type: 'video' }));
+			},
+			false
+		);
+	};
+}
